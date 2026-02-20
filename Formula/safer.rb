b90ac67f1e9065ca9ceca6ce1e204cecefc0d1c4
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2026-02-20 22:48:14 +0100

    Update formula to use the tar.gz

--- Formula/safer.rb
+++ Formula/safer.rb
@@ -1,9 +1,9 @@
 class Safer < Formula
   desc "safer: Protect your dev box from supply-chain attacks."
   homepage "https://github.com/fjfnaranjo/safer"
-  url "https://github.com/fjfnaranjo/safer/archive/refs/tags/release-0.1-rc2.zip"
+  url "https://github.com/fjfnaranjo/safer/archive/refs/tags/release-0.1-rc2.tar.gz"
   version "0.1-rc2"
-  sha256 "88614d6f044259bce300633bdc5df4fc2d745a94c7eafef65b59e7328aba32c2"
+  sha256 "21afbfd79f4f598d43ee95cc5849d2216e8f40a65c82ac8c115412eab20ab169"
 
   def install
     bin.install "safer-release-0.1-rc2/safer"
