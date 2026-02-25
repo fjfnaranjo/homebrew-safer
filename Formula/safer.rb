a5ac26cb6deffbb17d785dab2d842539bb7a6cd0
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2026-02-25 22:38:42 +0100

    Test some changes to the formula

--- Formula/safer.rb
+++ Formula/safer.rb
@@ -2,10 +2,12 @@ class Safer < Formula
   desc "safer: Protect your dev box from supply-chain attacks."
   homepage "https://fjfnaranjo.com/safer/"
   url "https://github.com/fjfnaranjo/safer/archive/refs/tags/release-0.1-rc3.tar.gz"
-  version "0.1-rc3"
   sha256 "75e0dac41c49d88f337aceee02a74dbb781a973145b9d0ab8f085882ae84963e"
 
+  revision 1
+
   def install
+    inreplace "safer", "0.0-dev", stable.version.to_s
     bin.install "safer"
     man1.install "safer.1"
   end
