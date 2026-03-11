2e9f35474a4a11a4b3532f9740a75cee3298573d
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2026-03-11 17:15:31 +0100

    Release 0.1-rc6

--- Formula/safer.rb
+++ Formula/safer.rb
@@ -1,11 +1,18 @@
 class Safer < Formula
   desc "safer: Protect your dev box from supply-chain attacks."
   homepage "https://fjfnaranjo.com/safer/"
-  url "https://github.com/fjfnaranjo/safer/releases/download/release-0.1-rc5/safer-0.1-rc5.tar.gz"
-  sha256 "7d48e8d5d9e749b6d8baf8acac111ba7881f96b80a00c617d21f95b4f9233468"
+  url "https://github.com/fjfnaranjo/safer/releases/download/release-0.1-rc6/safer-0.1-rc6.tar.gz"
+  sha256 "ea3a38b9f4c174e74f5cf19aed60cca43af8009cd8927ada4b1bedba24e76ebb"
 
   def install
+    inreplace "bin/safer",
+      "~/.local/share/safer:/usr/share/safer:/usr/local/share/safer",
+      "~/.local/share/safer:#{pkgshare}"
+
     bin.install "bin/safer"
     man1.install "share/man/man1/safer.1"
+
+    pkgshare.install "share/safer/tools"
+    pkgshare.install "share/safer/images"
   end
 end
