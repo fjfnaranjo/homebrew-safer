829ed56abdbca89a16ff416c3796dd10604e5074
Francisco José Fernández Naranjo <fjfnaranjo@gmail.com>
2026-08-07 22:29:07 +0200

    Release 0.1-rc10

--- Formula/safer.rb
+++ Formula/safer.rb
@@ -1,8 +1,8 @@
 class Safer < Formula
   desc "safer: Protect your dev box from supply-chain attacks."
   homepage "https://fjfnaranjo.github.io/safer/"
-  url "https://github.com/fjfnaranjo/safer/releases/download/release-0.1-rc9/safer-0.1-rc9.tar.gz"
-  sha256 "782c5af96fc2d175c4e2c5ad0fdc8d6679a8c671f826d764c5f05357f7b749d9"
+  url "https://github.com/fjfnaranjo/safer/releases/download/release-0.1-rc10/safer-0.1-rc10.tar.gz"
+  sha256 "8bdc19ad28d0867d1c49af2e54a588e91fc993284151ed58b7882a7e2c355b45"
 
   def install
     inreplace "bin/safer",
